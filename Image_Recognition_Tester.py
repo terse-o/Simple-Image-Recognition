e4da040013975460addabf9fd562dbef60696338
terse-o <52228002+terse-o@users.noreply.github.com>
2019-11-25 20:26:57 -0500

    Add files via upload

---
+++ Image_Recognition_Tester.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import numpy as np
+from keras.models import load_model
+
+labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+# loading trained model
+model = load_model('Trained_model.h5')
+
+input_path = input('Enter image file pathname: ')
+input_image = Image.open(input_path)
+input_image = input_image.resize((32, 32), resample=Image.LANCZOS)
+image_array = np.array(input_image)
+image_array = image_array.astype('float32')
+image_array /= 255.0
+image_array = image_array.reshape(1, 32, 32, 3)
+answer = model.predict(image_array)
+
+# show model recreated image
+# input_image.show()
+
+# output predicted label
+print(labels[np.argmax(answer)])
